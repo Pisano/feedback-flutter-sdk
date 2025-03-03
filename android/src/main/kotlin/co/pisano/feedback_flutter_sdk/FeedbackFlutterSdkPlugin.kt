150652fa3dc11f98d0f7f038310939900f9a21ff
canser.ufuk.isler <41027440+canserufukisler@users.noreply.github.com>
2025-03-03 17:18:37 +0300

    Android module fix
    Fix for "Reply already submitted" error

--- android/src/main/kotlin/co/pisano/feedback_flutter_sdk/FeedbackFlutterSdkPlugin.kt
+++ android/src/main/kotlin/co/pisano/feedback_flutter_sdk/FeedbackFlutterSdkPlugin.kt
@@ -25,6 +25,7 @@ class FeedbackFlutterSdkPlugin : FlutterPlugin, MethodCallHandler {
     private lateinit var channel: MethodChannel
     private lateinit var context: Context
     private var actionCallback: ((PisanoActions) -> Unit)? = null
+    private var isResultReturned = false
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "feedback_flutter_sdk")
@@ -33,93 +34,117 @@ class FeedbackFlutterSdkPlugin : FlutterPlugin, MethodCallHandler {
     }
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-        when (call.method) {
-            "getPlatformVersion" -> result.success("Android ${android.os.Build.VERSION.RELEASE}")
-            "init" -> {
-                val manager = PisanoSDKManager.Builder(context)
-                    .setApplicationId(call.argument<String>("applicationId"))
-                    .setAccessKey(call.argument<String>("accessKey"))
-                    .setApiUrl(call.argument<String>("apiUrl"))
-                    .setFeedbackUrl(call.argument<String>("feedbackUrl"))
-                    .setEventUrl(call.argument<String?>("eventUrl"))
-                    .setCloseStatusCallback(object : ActionListener {
-                        override fun action(action: PisanoActions) {
-                            actionCallback?.invoke(action)
+        isResultReturned = false
+
+        try {
+            when (call.method) {
+                "getPlatformVersion" -> {
+                    result.success("Android ${android.os.Build.VERSION.RELEASE}")
+                }
+
+                "init" -> {
+                    val manager = PisanoSDKManager.Builder(context)
+                        .setApplicationId(call.argument<String>("applicationId"))
+                        .setAccessKey(call.argument<String>("accessKey"))
+                        .setApiUrl(call.argument<String>("apiUrl"))
+                        .setFeedbackUrl(call.argument<String>("feedbackUrl"))
+                        .setEventUrl(call.argument<String?>("eventUrl"))
+                        .setCloseStatusCallback(object : ActionListener {
+                            override fun action(action: PisanoActions) {
+                                actionCallback?.invoke(action)
+                            }
+                        })
+                        .build()
+                    PisanoSDK.init(manager)
+                    result.success(null)
+                }
+
+                "show" -> {
+                    val viewMode: ViewMode = ViewMode.parse(call.argument<Int>("viewMode") ?: 0) ?: ViewMode.DEFAULT
+                    val payload = call.argument<HashMap<String, String>>("payload")
+                    val customer = call.argument<HashMap<String, Any>>("customer")
+
+                    val titleArg = call.argument<String?>("title")
+                    val title = if (!titleArg.isNullOrBlank()) {
+                        Title(text = titleArg, textSize = call.argument<Int?>("titleFontSize")?.toFloat())
+                    } else {
+                        null
+                    }
+
+                    val customerModel = PisanoCustomer(
+                        name = customer?.get("name") as? String,
+                        externalId = customer?.get("externalId") as? String,
+                        email = customer?.get("email") as? String,
+                        phoneNumber = customer?.get("phoneNumber") as? String,
+                        customAttributes = customer?.get("customAttrs") as? HashMap<String, Any>
+                    )
+
+                    PisanoSDK.show(
+                        viewMode = viewMode,
+                        title = title,
+                        flowId = call.argument<String?>("flowId"),
+                        language = call.argument<String?>("language"),
+                        payload = payload,
+                        pisanoCustomer = customerModel
+                    )
+
+                    actionCallback = { action ->
+                        if (!isResultReturned && action != PisanoActions.OPENED) {
+                            result.success(action.ordinal + 1)
+                            isResultReturned = true
+                            actionCallback = null
                         }
-                    })
-                    .build()
-                PisanoSDK.init(manager)
-            }
-            "show" -> {
-                val viewMode: ViewMode = ViewMode.parse(call.argument<Int>("viewMode") ?: 0) ?: ViewMode.DEFAULT
-                var title: Title? = null
-                val payload = call.argument<HashMap<String, String>>("payload")
-                val customer = call.argument<HashMap<String, Any>>("customer")
-
-                val titleArg = call.argument<String?>("title")
-                if (!titleArg.isNullOrBlank()) {
-                    title = Title(text = titleArg, textSize = call.argument<Int?>("titleFontSize")?.toFloat())
-                }
-
-                val customerModel = PisanoCustomer(
-                    name = customer?.get("name") as? String,
-                    externalId = customer?.get("externalId") as? String,
-                    email = customer?.get("email") as? String,
-                    phoneNumber = customer?.get("phoneNumber") as? String,
-                    customAttributes = customer?.get("customAttrs") as? java.util.HashMap<String, Any>
-                )
-
-                PisanoSDK.show(
-                    viewMode = viewMode,
-                    title = title,
-                    flowId = call.argument<String?>("flowId"),
-                    language = call.argument<String?>("language"),
-                    payload = payload,
-                    pisanoCustomer = customerModel
-                )
-
-                actionCallback = {
-                    if (it != PisanoActions.OPENED) {
-                        result.success(it.ordinal + 1)
                     }
                 }
-            }
-            "track" -> {
-                val props = call.argument<HashMap<String, Any>>("props")
-                val payload = call.argument<HashMap<String, String>>("payload")
-                val customer = call.argument<HashMap<String, Any>>("customer")
 
-                val customAttributes = java.util.HashMap<String, Any>()
-                customer?.keys?.forEach { key ->
-                    customAttributes[key] = customer?.get(key) as Any
-                }
+                "track" -> {
+                    val props = call.argument<HashMap<String, Any>>("props")
+                    val payload = call.argument<HashMap<String, String>>("payload")
+                    val customer = call.argument<HashMap<String, Any>>("customer")
 
-                val customerModel = PisanoCustomer(
-                    name = customer?.get("name") as? String,
-                    externalId = customer?.get("externalId") as? String,
-                    email = customer?.get("email") as? String,
-                    phoneNumber = customer?.get("phoneNumber") as? String,
-                    customAttributes = customAttributes
-                )
+                    val customAttributes = HashMap<String, Any>()
+                    customer?.keys?.forEach { key ->
+                        customAttributes[key] = customer[key] as Any
+                    }
 
-                PisanoSDK.track(
-                    event = (props?.get("event") as? String) ?: "",
-                    payload = payload,
-                    pisanoCustomer = customerModel
-                )
+                    val customerModel = PisanoCustomer(
+                        name = customer?.get("name") as? String,
+                        externalId = customer?.get("externalId") as? String,
+                        email = customer?.get("email") as? String,
+                        phoneNumber = customer?.get("phoneNumber") as? String,
+                        customAttributes = customAttributes
+                    )
 
-                actionCallback = {
-                    if (it != PisanoActions.OPENED) {
-                        result.success(it.ordinal + 1)
+                    PisanoSDK.track(
+                        event = (props?.get("event") as? String) ?: "",
+                        payload = payload,
+                        pisanoCustomer = customerModel
+                    )
+
+                    actionCallback = { action ->
+                        if (!isResultReturned && action != PisanoActions.OPENED) {
+                            result.success(action.ordinal + 1)
+                            isResultReturned = true
+                            actionCallback = null
+                        }
                     }
                 }
+
+                "clear" -> {
+                    PisanoSDK.clearAction()
+                    result.success(null)
+                }
+
+                else -> {
+                    result.notImplemented()
+                }
             }
-            "clear" -> {
-                PisanoSDK.clearAction()
-            }
-            else -> result.notImplemented()
+        } catch (e: Exception) {
+            Log.e("FeedbackFlutterSdk", "An error occured: ${e.message}", e)
+            result.error("ERROR", "An error occured during the process: ${e.message}", null)
         }
     }
+
 
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
